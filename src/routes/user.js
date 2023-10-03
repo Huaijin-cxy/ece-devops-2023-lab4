e1ffba8dad00209136fe1aa0deb804da844cb33b
Xinyue Cheng <13523092590@163.com>
2023-10-03 11:20:07 +0200

    first commit

---
+++ src/routes/user.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const userController = require('../controllers/user')
+
+const userRouter = express.Router()
+
+userRouter
+  .post('/', (req, resp) => {
+    userController.create(req.body, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
+        }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
+    })
+  })
+  .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+    // TODO Create get method API
+    const user_name = req.params.username;
+
+  // Call the userController's get method to retrieve the user by username
+  userController.get(user_name, (err, user) => {
+    let respObj;
+    
+    if (err) {
+      respObj = {
+        status: "error",
+        msg: err.message
+      };
+      return resp.status(400).json(respObj);
+    }
+    
+    respObj = {
+      status: "success",
+      msg: user
+    };
+    
+    resp.status(200).json(respObj);
+  });
+  })
+  
+module.exports = userRouter
